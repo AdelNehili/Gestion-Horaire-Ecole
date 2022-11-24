ef7c27d97612aa1bf5fa42330f49131aff3e0e82
AdelNehili <49352113+AdelNehili@users.noreply.github.com>
2022-11-24 22:16:47 +0100

    Update main.py
    Only comments

--- gestion/main.py
+++ gestion/main.py
@@ -7,4 +7,5 @@ master.setup()
 if __name__ == "__main__":
     table = master.convert_color_hours()
     for element in table:
+        #Après le travail de génération d'horaire, on affiche l'identifiant du cours/son jour/son heure.
         print(element)
